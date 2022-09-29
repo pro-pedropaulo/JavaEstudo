fdf06537e0f7be2ad3c21518d8de7f38217838ad
pro-pedropaulo <pedro.pbcs@hotmail.com>
2022-09-29 14:39:38 -0300

    testando nova mudança

--- projetos/testandoGit.java
+++ projetos/testandoGit.java
@@ -4,6 +4,10 @@ public class testandoGit {
 
     public static void main(String[] args) {
         System.out.println("Testando Git");
+
+        for (int i = 1; i <= 100; i+=2) {
+            System.out.println(i);
+        }
     }
 }
 
