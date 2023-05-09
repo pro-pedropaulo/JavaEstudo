c5ffdf60bcd108d5340b07b6d4e0a0a684764237
Pedro Paulo <pedro.pbcs@hotmail.com>
2023-05-08 23:44:16 -0300

    trazendo quase no local certo mas com letra branca

--- ProjetoOCR/src/main/java/com/meu/projeto/App.java
+++ ProjetoOCR/src/main/java/com/meu/projeto/App.java
@@ -3,22 +3,23 @@ package com.meu.projeto;
 import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.*;
-import com.itextpdf.layout.Document;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.layout.Canvas;
 import com.itextpdf.layout.element.Paragraph;
-import com.itextpdf.layout.property.TextAlignment;
-import com.itextpdf.layout.property.VerticalAlignment;
+import com.itextpdf.layout.element.Text;
+import net.sourceforge.tess4j.ITessAPI;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.Word;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.rendering.PDFRenderer;
-import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -46,25 +47,21 @@ public class App {
             instance.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
             instance.setLanguage("por");
 
-            File inputFileOCR = new File(inputFile);
-            String ocrResult = instance.doOCR(inputFileOCR);
+            List<Word> words = instance.getWords(image, ITessAPI.TessPageIteratorLevel.RIL_WORD);
 
             // Ler arquivo temporário com iText 7
             PdfReader reader = new PdfReader(tempPdf);
             PdfWriter writer = new PdfWriter(outputPdf);
             PdfDocument pdfDocument = new PdfDocument(reader, writer);
-            Document document = new Document(pdfDocument);
 
-            // Adicionar o conteúdo OCR como texto transparente
-            Paragraph paragraph = new Paragraph(ocrResult)
-                    .setFontColor(new DeviceRgb(255, 255, 255))
-                    .setFontSize(10)
-                    .setTextAlignment(TextAlignment.LEFT)
-                    .setVerticalAlignment(VerticalAlignment.TOP)
-                    .setFixedPosition(1, 0, pdfDocument.getFirstPage().getPageSize().getHeight(), pdfDocument.getFirstPage().getPageSize().getWidth());
+            // Adicionar o conteúdo OCR como texto transparente nas posições específicas
+            for (Word word : words) {
+                Rectangle rect = new Rectangle((float) word.getBoundingBox().getX(), (float) (pdfDocument.getFirstPage().getPageSize().getHeight() - word.getBoundingBox().getY() - word.getBoundingBox().getHeight()), (float) word.getBoundingBox().getWidth(), (float) word.getBoundingBox().getHeight());
+                Text text = new Text(word.getText()).setFontColor(new DeviceRgb(255, 255, 255)).setFontSize((float) word.getBoundingBox().getHeight());
+                new Canvas(new PdfCanvas(pdfDocument.getFirstPage()), pdfDocument, rect).add(new Paragraph(text));
+            }
 
-            document.add(paragraph);
-            document.close();
+            pdfDocument.close();
 
         } catch (Exception e) {
             e.printStackTrace();
