9c9786bad6214149e6f0bec5691f48bd898cad6d
Pedro Paulo <pedro.pbcs@hotmail.com>
2023-05-09 00:54:08 -0300

    gerando pdf pesquisavel

--- ProjetoOCR/src/main/java/com/meu/projeto/App.java
+++ ProjetoOCR/src/main/java/com/meu/projeto/App.java
@@ -1,10 +1,13 @@
 package com.meu.projeto;
 
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.*;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.xobject.PdfImageXObject;
 import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Text;
 import com.itextpdf.layout.property.Property;
@@ -17,65 +20,69 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.InputStream;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         try {
-            // Converter imagem PNG em PDF
-            String inputFile = "C:\\testeImagem\\wiki.png";
-            String tempPdf = "C:\\testeImagem\\temp.pdf";
+            File folder = new File("C:\\testeImagem\\");
+            File[] files = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".png"));
 
-            BufferedImage image = ImageIO.read(new File(inputFile));
-            PDDocument doc = new PDDocument();
-            PDPage page = new PDPage(new PDRectangle(image.getWidth(), image.getHeight()));
-            doc.addPage(page);
-
-            PDPageContentStream contentStream = new PDPageContentStream(doc, page);
-            contentStream.drawImage(org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory.createFromImage(doc, image), 0, 0, image.getWidth(), image.getHeight());
-            contentStream.close();
-
-            doc.save(tempPdf);
-            doc.close();
-
-            // Executar OCR
-            String outputPdf = "C:\\testeImagem\\output.pdf";
-
-            ITesseract instance = new Tesseract();
-            instance.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
-            instance.setLanguage("por");
-
-            List<Word> words = instance.getWords(image, ITessAPI.TessPageIteratorLevel.RIL_WORD);
-
-            // Ler arquivo temporário com iText 7
-            PdfReader reader = new PdfReader(tempPdf);
-            PdfWriter writer = new PdfWriter(outputPdf);
-            PdfDocument pdfDocument = new PdfDocument(reader, writer);
-
-            // Adicionar o conteúdo OCR como texto invisível nas posições específicas
-            TransparentColor invisibleColor = new TransparentColor(new DeviceRgb(0, 0, 0), 0);
-            for (Word word : words) {
-                float wordHeight = (float) word.getBoundingBox().getHeight();
-                Rectangle rect = new Rectangle((float) word.getBoundingBox().getX(), (float) (pdfDocument.getFirstPage().getPageSize().getHeight() - word.getBoundingBox().getY() - wordHeight), (float) word.getBoundingBox().getWidth(), wordHeight);
-                Text text = new Text(word.getText()).setFontSize(wordHeight);
-                text.setProperty(Property.FONT_COLOR, invisibleColor);
-                text.setProperty(Property.STROKE_COLOR, invisibleColor);
-                Paragraph paragraph = new Paragraph(text);
-                paragraph.setMarginTop(-wordHeight *- 0.35f); // Ajuste a proporção conforme necessário
-                paragraph.setFixedLeading(wordHeight);
-                paragraph.setMultipliedLeading(0);
-
-                new Canvas(new PdfCanvas(pdfDocument.getFirstPage()), pdfDocument, rect).add(paragraph);
+            if (files == null || files.length == 0) {
+                System.out.println("Nenhum arquivo PNG encontrado na pasta.");
+                return;
             }
 
+            // Criar um novo documento PDF
+            PdfDocument pdfDocument = new PdfDocument(new PdfWriter("C:\\testeImagem\\output.pdf"));
 
+            for (File file : files) {
+                // Carregar imagem
+                BufferedImage image = ImageIO.read(file);
 
+                // Converter imagem em um objeto PDF
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(image, "png", baos);
+                InputStream inputStream = new ByteArrayInputStream(baos.toByteArray());
+                PdfImageXObject pdfImageXObject = new PdfImageXObject(ImageDataFactory.create(baos.toByteArray()));
+                
+                // Adicionar a imagem ao documento PDF
+                PdfPage pdfPage = pdfDocument.addNewPage(new com.itextpdf.kernel.geom.PageSize(image.getWidth(), image.getHeight()));
+                PdfCanvas pdfCanvas = new PdfCanvas(pdfPage);
+                pdfCanvas.addXObjectAt(pdfImageXObject, 0, 0);
 
+                // Executar OCR na imagem atual
+                ITesseract instance = new Tesseract();
+                instance.setDatapath("C:\\Program Files\\Tesseract-OCR\\tessdata");
+                instance.setLanguage("por");
+                List<Word> words = instance.getWords(image, ITessAPI.TessPageIteratorLevel.RIL_WORD);
 
+                // Adicionar o conteúdo OCR como texto invisível nas posições específicas
+                TransparentColor invisibleColor = new TransparentColor(new DeviceRgb(0, 0, 0), 0);
+                for (Word word : words) {
+                    float wordHeight = (float) word.getBoundingBox().getHeight();
+                    Rectangle rect = new Rectangle((float) word.getBoundingBox().getX(), (float) (pdfPage.getPageSize().getHeight() - word.getBoundingBox().getY() - wordHeight), (float) word.getBoundingBox().getWidth(), wordHeight);
+                    Text text = new Text(word.getText()).setFontSize(wordHeight);
+                    text.setProperty(Property.FONT_COLOR, invisibleColor);
+                    text.setProperty(Property.STROKE_COLOR, invisibleColor);
+                    Paragraph paragraph = new Paragraph(text);
+                    paragraph.setMarginTop(-wordHeight * -0.35f); // Ajuste a proporção conforme necessário
+                    paragraph.setFixedLeading(wordHeight);
+                    paragraph.setMultipliedLeading(0);
+
+                    new Canvas(new PdfCanvas(pdfPage), pdfDocument, rect).add(paragraph);
+                }
+            }
+
+            // Fechar os documentos
             pdfDocument.close();
 
         } catch (Exception e) {
@@ -83,5 +90,3 @@ public class App {
         }
     }
 }
-
-
